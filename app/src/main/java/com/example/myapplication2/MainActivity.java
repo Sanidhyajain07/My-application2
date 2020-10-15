88ed4200dbd81d5751339459471a6fada0184975
Sanidhya Jain <Sanidhyajain07>
2020-10-15 13:16:20 +0530

    Random Names

---
+++ app/src/main/java/com/example/myapplication2/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.myapplication2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final TextView textView=(TextView)findViewById(R.id.textView);
+        final Button button1=(Button)findViewById(R.id.button);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String[] names=getResources().getStringArray(R.array.name);
+                Random rand= new Random();
+                int n =rand.nextInt(names.length);
+                textView.setText(names[n]);
+            }
+        });
+
+    }
+}
